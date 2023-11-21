0d67b233f919e488059af44533a11e42a18b8896
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-21 14:51:35 +0900

    Complete

--- CLASS_2/2231_분해합.py
+++ CLASS_2/2231_분해합.py
@@ -3,13 +3,15 @@ N = int(input())
 N_list = []
 M_list = []
 for i in range(1, N+1):
-    N_list.append(str(i))
+    M_list.append(str(i))
     if len(str(i)) == 1:
-        M_list.append(i*2)
+        N_list.append(i*2)
     else:
         a = sum(list(map(int, list(str(i)))))
-        M_list.append(i + a)
+        N_list.append(i + a)
 
-if str(N) in N_list:
-    idx = N_list.index(str(N))
+if N in N_list:
+    idx = N_list.index(N)
     print(M_list[idx])
+else:
+    print(0)

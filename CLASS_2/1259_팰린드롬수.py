1ef386e70a1fe452b2c89a09403238144e418be4
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-22 19:36:28 +0900

    Complete

---
+++ CLASS_2/1259_팰린드롬수.py
@@ -0,0 +1,21 @@
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    else:
+        N_list = list(str(N))
+        cond = len(N_list) // 2
+        if len(N_list) % 2 == 1:
+            left_N = N_list[:cond]
+            right_N = N_list[cond+1:][::-1]
+            if left_N == right_N:
+                print('yes')
+            else:
+                print('no')
+        elif len(N_list) % 2 == 0:
+            left_N = N_list[:cond]
+            right_N = N_list[cond:][::-1]
+            if left_N == right_N:
+                print('yes')
+            else:
+                print('no')

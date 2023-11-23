206da3724d250b7d543db77f8f52caf95d33ce46
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-23 18:48:14 +0900

    Complete

--- CLASS_2/2609_최대공약수와최소공배수.py
+++ CLASS_2/2609_최대공약수와최소공배수.py
@@ -16,3 +16,21 @@ A, B = map(int, input().split())
 output1, output2 = gcd_lcm(A, B)
 print(output1)
 print(output2)
+
+
+# 유클리드 호제법
+def gcd(x, y):
+    while y > 0:
+        x, y = y, x % y
+    return x
+
+def lcm(x, y):
+    return int(x * y / gcd(x, y))
+
+A, B = map(int, input().split())
+if A < B:
+    A, B = B, A
+result1 = gcd(A, B)
+result2 = lcm(A, B)
+print(result1)
+print(result2)

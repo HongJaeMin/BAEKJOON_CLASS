411fc16955bfac8339a21f7708b92b27157fc1d2
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-23 20:54:06 +0900

    Complete

---
+++ CLASS_2/2869_달팽이는올라가고싶다.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+A, B, V = map(int, input().split())
+
+if (V - B) % (A - B) == 0:
+    day = (V - B) // (A - B)
+elif (V - B) % (A - B) != 0:
+    day = (V - B) // (A - B) + 1
+print(day)
+
+# 아래는 시간 초과
+# day = 0
+# while True:
+#     day += 1
+#     V = V - (A - B)
+#     if V <= A:
+#         day += 1
+#         break
+# print(day)

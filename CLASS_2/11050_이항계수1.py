8c0dc9e1dfc63e1e7d880a875cf5873851daa467
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-23 21:26:59 +0900

    Complete

---
+++ CLASS_2/11050_이항계수1.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+
+x = y = z = 1
+for i in range(1, (N - K) + 1):
+    x *= i
+for i in range(1, K + 1):
+    y *= i
+for i in range(1, N + 1):
+    z *= i
+print(int(z / (x * y)))
+
+
+# math 모듈
+import math
+
+print(math.comb(N, K))

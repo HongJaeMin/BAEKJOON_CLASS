2363a0534df1e3e41a9ff3395e610b287ced8f2d
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-11-25 02:59:38 +0900

    Complete

--- CLASS_2/10989_수정렬하기3.py
+++ CLASS_2/10989_수정렬하기3.py
@@ -3,9 +3,19 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 
-L = []
+L = [0] * 10001
 for _ in range(N):
     num = int(input())
-    L.append(num)
-sorted_list = sorted(L)
-print(sorted_list)
+    L[num] += 1
+for i in range(0, len(L)):
+    if L[i] != 0:
+        for _ in range(L[i]):
+            print(i)
+
+# 메모리 초과
+# L = []
+# for _ in range(N):
+#     num = int(input())
+#     L.append(num)
+# sorted_list = sorted(L)  # L.sort()
+# print(sorted_list)  # print(L)

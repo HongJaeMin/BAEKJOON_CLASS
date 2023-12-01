045d8ac1797d9d85735890105cbbd73186e07dea
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-12-01 21:33:48 +0900

    Incomplete

--- CLASS_2/4949_균형잡힌세상.py
+++ CLASS_2/4949_균형잡힌세상.py
@@ -1,1 +1,22 @@
 # Incomplete
+import sys, re
+input = sys.stdin.readline
+
+while True:
+    inp = input().lower().strip('\n')
+
+    cond = {'(', ')', '[', ']', ' '}
+    if any(a in inp for a in cond):
+        inp_re = re.sub(r'[a-z ]', '', inp)
+        condd = {'()', '[]'}
+        while any(a in inp_re for a in condd):
+            for a in condd:
+                if a in inp_re:
+                    inp_re = inp_re.replace(a, '')
+
+        if inp_re == '.':
+            print('yes')
+        else:
+            print('no')
+    else:
+        break

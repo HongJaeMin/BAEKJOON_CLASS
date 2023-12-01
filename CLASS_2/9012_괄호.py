4f7f872d1fce3f35e759500a17791e336aa92f1c
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-12-01 21:34:26 +0900

    Incomplete

---
+++ CLASS_2/9012_괄호.py
@@ -0,0 +1,12 @@
+# Incomplete
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    inp = [''.join(list(input().strip()))]
+    print(inp)
+
+# 책 공부
+# 완전탐색, 시뮬레이션, 구현(104p, 320p), DFS/BFS(124p, 338p)

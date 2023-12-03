088c0121f3b6cce0df0314370a286a14a6185f35
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-12-03 18:36:51 +0900

    Complete

--- CLASS_2/9012_괄호.py
+++ CLASS_2/9012_괄호.py
@@ -1,12 +1,25 @@
-# Incomplete
 import sys
 
 input = sys.stdin.readline
 T = int(input())
 
 for _ in range(T):
-    inp = [''.join(list(input().strip()))]
-    print(inp)
+    inp = input().rstrip()
+    stack = []
+    cnt = 0
 
-# 책 공부
-# 완전탐색, 시뮬레이션, 구현(104p, 320p), DFS/BFS(124p, 338p)
+    for i in inp:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if not stack or stack[-1] == ')':
+                cnt = 1
+                print('NO')
+                break
+            else:
+                stack.pop()
+    if cnt == 0:
+        if not stack:
+            print('YES')
+        else:
+            print('NO')

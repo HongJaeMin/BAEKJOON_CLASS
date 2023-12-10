c7ecff7277fe886ef71f00fd9421564ebceabf55
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2023-12-10 21:09:41 +0900

    Complete

--- CLASS_2/2775_부녀회장이될테야.py
+++ CLASS_2/2775_부녀회장이될테야.py
@@ -1,1 +1,19 @@
-# Incomplete
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+
+    f0 = [_ for _ in range(1, n+1)]
+    i = 0
+    while i < k:
+        fn = []
+        j = 0
+        while j < n:
+            fn.append(sum(f0[:j+1]))
+            j += 1
+        f0 = fn
+        i += 1
+    print(fn[n-1])
